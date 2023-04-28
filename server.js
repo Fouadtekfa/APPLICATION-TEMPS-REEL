63e5aef5562fe198a1d77953e41ffdf302c80cf6
Fouadtekfa <tekfouad06@gmail.com>
2023-04-28 19:49:42 +0200

    ecoute de message de cote server fonctionnel

--- server.js
+++ server.js
@@ -3,7 +3,19 @@ var app=require("express")();
 var http=require("http").Server(app);
 var io=require('socket.io')(http);
 
-
+//get de fichiers html client dans le serveur
+app.get("",function (req,res){
+   res.sendFile(__dirname+'/index.html');
+});
+io.on("connection",function (socket){
+   console.log("a user is connected ");
+   socket.on("disconnect",function (){
+      console.log('a user is disconnected');
+   });
+   socket.on("chat message ",function (){
+      console.log('message recu '+msg);
+   });
+});
 //lancer le serveur
 http.listen(3000,function (){
    console.log(" run on  http://localhost:3000/")
